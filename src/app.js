dbdaca63cca311ed7f228fa271f7b9e01db4c5e5
damian <git2507>
2021-06-30 17:29:03 +0200

    Nuevas dependencias

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
+//import Bootstrap from 'bootstrap'
 import BootstrapVue from 'bootstrap-vue'
-Vue.use(BootstrapVue)
-
-import '@babel/polyfill'
-import 'mutationobserver-shim'
+//import '@babel/polyfill'
+//import 'mutationobserver-shim'
 import './plugins/bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
 
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+//window.axios = require('axios');
+//window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+//const axios = require('axios');
 
 // My app
 import router from './router/index'
