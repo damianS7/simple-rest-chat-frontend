19e8c25a464c2029b35b217e2c87d9c340379225
damian <git2507>
2021-07-01 15:54:32 +0200

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,84 @@
 <template>
   <b-container id="app" fluid>
-    <b-row
-      v-if="!isLogged"
-      id="login-component"
-      class="full-screen-component bg-secondary"
-    >
+    <b-row v-if="!isLogged" id="login-component" class="full-screen-component">
       <login></login>
     </b-row>
+    <b-row id="chat-component" class="full-screen-component">
+      <b-row class="app-one">
+        <b-col cols="5" sm="5" md="5" lg="3" class="side">
+          <div class="side-left">
+            <b-row class="heading">
+              <b-col cols="5" sm="3" class="heading-avatar">
+                <div class="heading-avatar-icon">
+                  <img @click="showProfile" src="a.jpg" />
+                </div>
+              </b-col>
 
-    <b-row id="chat-component" class="full-screen-component bg-primary">
-      <chat></chat>
+              <b-col cols="7" sm="5" class="heading-name">
+                <a class="heading-name-meta">{{ appUser.name }}</a>
+              </b-col>
+            </b-row>
+
+            <b-row class="m-0">
+              <b-col class="side">
+                <conversation-list></conversation-list>
+                <room-list></room-list>
+              </b-col>
+            </b-row>
+          </div>
+          <div class="side-profile">
+            <profile></profile>
+          </div>
+        </b-col>
+
+        <b-col cols="7" sm="7" md="7" lg="9" class="conversation">
+          <b-row class="heading">
+            <b-col cols="12" class="heading-name">
+              <a v-if="selectedConversationTitle" class="heading-name-meta">{{
+                selectedConversationTitle
+              }}</a>
+            </b-col>
+          </b-row>
+
+          <b-row id="conversation-box" class="m-0">
+            <conversation-history></conversation-history>
+          </b-row>
+        </b-col>
+      </b-row>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import Chat from "./components/Chat.vue";
+import { mapGetters, mapState } from "vuex";
 import Login from "./components/Login.vue";
-import { mapGetters } from "vuex";
+import Profile from "./components/Profile.vue";
+import ConversationList from "./components/ConversationList.vue";
+import ConversationHistory from "./components/ConversationHistory.vue";
 export default {
   name: "App",
   components: {
     Login,
-    Chat,
+    Profile,
+    ConversationList,
+    ConversationHistory,
   },
   computed: {
     ...mapGetters({
       isLogged: "appuser/isLogged",
       appReady: "appReady",
     }),
+    ...mapState({
+      appUser: (state) => state.appuser.appUser,
+      selectedChat: (state) => state.selectedChat,
+    }),
+    selectedConversationTitle: function () {
+      if (this.selectedConversation != null) {
+        return this.selectedConverasation.name;
+      }
+
+      return "";
+    },
   },
   methods: {
     showLogin() {
@@ -39,11 +89,21 @@ export default {
       var div = document.getElementById("login-component");
       div.style.left = "-100%";
     },
+    showProfile() {
+      var div = document.getElementsByClassName("side-profile")[0];
+      div.style.left = "0%";
+    },
   },
 };
 </script>
-
 <style>
+@import "./assets/css/chat.css";
+.app-one {
+  width: 100%;
+}
+#conversation-box {
+  height: calc(100% - 60px);
+}
 #app {
   text-align: center;
   color: #2c3e50;
@@ -61,7 +121,7 @@ export default {
 }
 
 #login-component {
-  z-index: 10;
+  z-index: 99999;
   left: 0;
   position: absolute;
   -webkit-transition: left 2s ease;
--- src/store/index.js
+++ src/store/index.js
@@ -3,64 +3,49 @@ import Vue from "vue"
 
 // Modules
 import appuser from "./modules/user"
-import room from "./modules/room"
 import conversation from "./modules/conversation"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        appuser, room, conversation
+        appuser, conversation
     },
     state: {
         // Al inicio de la aplicacion (mientras se carga al abrir la url)
         // la app no puede ser usada porque los datos no se han cargado
         // por lo que usaremos el flag para determinar si la app termino de cargar
         ready: false,
-        selectedUser: {},
-        selectedChat: { room: null, conversation: null }
+        rooms: [],
+        selectedConversation: {
+            id: 1,
+            name: 'Pepito',
+            isRoom: false,
+            messages: [
+                { senderId: 1, sender: 'Pepito', text: 'Hola' },
+                { senderId: 1, sender: 'Pepito', text: 'Xd' },
+                { senderId: 1, sender: 'Pepito', text: 'Estas?' }
+
+            ]
+        }
     },
     getters: {
         appReady: (state) => {
             return state.ready;
         },
-        lastChatSelected: (state) => {
-            if (state.selectedChat.room != null) {
-                return { room: state.selectedChat.room }
-            }
-
-            if (state.selectedChat.conversation != null) {
-                return { conversation: state.selectedChat.conversation }
-            }
-
-            return null;
-        }
     },
     mutations: {
         SET_READY(state) {
             state.ready = true;
         },
         // Selecciona una conversacion
-        SET_ROOM_CHAT(state, room) {
-            state.selectedChat.room = room;
-        },
-        SET_CONVERSATION_CHAT(state, conversation) {
-            state.selectedChat.conversation = conversation;
+        SET_SELECTED_CONVERSATION(state, conversation) {
+            state.selectedConversation = conversation;
         },
     },
     actions: {
-        selectConversationById(context, conversationId) {
-            //var conversation = context.getters.getConversationById(conversationId);
-            //context.commit('SET_SELECTED_CONVERSATION', conversation);
-        },
         selectConversation(context, conversation) {
-            context.commit('SET_ROOM_CHAT', null);
-            context.commit('SET_CONVERSATION_CHAT', conversation);
-        },
-        selectRoom(context, room) {
-            context.commit('SET_ROOM_CHAT', room);
-            context.commit('SET_CONVERSATION_CHAT', null);
-            context.room.state.selectedRoom = room;
+            context.commit('SET_SELECTED_CONVERSATION', conversation);
         },
     }
 })
