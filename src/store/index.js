2163eecc7ff44b58b96b491c731db3f7d957c39f
damian <git2507>
2021-07-01 16:04:45 +0200

    rework

--- src/store/index.js
+++ src/store/index.js
@@ -15,8 +15,12 @@ export default new Vuex.Store({
         // Al inicio de la aplicacion (mientras se carga al abrir la url)
         // la app no puede ser usada porque los datos no se han cargado
         // por lo que usaremos el flag para determinar si la app termino de cargar
-        ready: false,
+        appReady: false,
+
+        // Lista con todas las salas
         rooms: [],
+
+        // Puede ser una sala o conversacion con otro usuario
         selectedConversation: {
             id: 1,
             name: 'Pepito',
@@ -30,22 +34,62 @@ export default new Vuex.Store({
         }
     },
     getters: {
-        appReady: (state) => {
-            return state.ready;
+        isAppReady: (state) => {
+            return state.appReady;
         },
+        getSelectedConversation: (state) => {
+            return state.selectedConversation;
+        },
+        getConversation: (state) => {
+            return state.conversation;
+        }
     },
     mutations: {
         SET_READY(state) {
-            state.ready = true;
+            state.appReady = true;
         },
         // Selecciona una conversacion
         SET_SELECTED_CONVERSATION(state, conversation) {
             state.selectedConversation = conversation;
         },
+        SET_READY(state) {
+            state.ready = true;
+        },
+        ADD_CONVERSATION(state, conversation) {
+            state.rooms.push(conversation);
+        },
+        SET_CONVERSATIONS(state, conversations) {
+            state.rooms = conversations;
+        },
     },
     actions: {
         selectConversation(context, conversation) {
             context.commit('SET_SELECTED_CONVERSATION', conversation);
+        },
+        addRoom(context, conversation) {
+            let newConversation = {
+                id: conversation.id,
+                name: conversation.name,
+                isRoom: false,
+                messages: [
+                    // Uusado para el mensaje inicial
+                    {
+                        senderId: null,
+                        sender: null,
+                        message: 'Welcome to the chat.'
+                    }
+                ]
+            }
+            context.commit('ADD_CONVERSATION', newConversation);
+        },
+        addRooms(context, conversations) {
+            //rooms.array.forEach(room => {
+            //context.addRoom(room);
+            //});
+            context.commit('SET_CONVERSATIONS', conversations);
+        },
+        addMessage(context, conversation) {
+            // Comprobar si es una room o conversacion entre usuarios
         },
     }
 })
