d90141854473dc97d26699b188e24b8b3cef049e
damian <git2507>
2021-07-01 21:08:04 +0200

    Eliminado codigo innecesario

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -12,18 +12,10 @@
       </b-row>
     </b-row>
 
-    <b-row class="composeBox h-auto">
-      <b-col cols="12" class="composeBox-inner heading-avatar h-auto">
-        <div class="profile-avatar-icon h-auto">
-          <img v-b-modal.modal-center class="img-fluid" :src="avatarPath" />
-        </div>
-      </b-col>
-    </b-row>
-
     <div class="compose-sideBar">
       <div class="compose-sideBar myhc">
         <b-row class="sideBar-profile">
-          <b-col cols="12">Alias:</b-col>
+          <b-col cols="12">Name</b-col>
           <b-col cols="12">
             <input
               type="text"
@@ -32,31 +24,19 @@
               @change="updateProfile"
             />
           </b-col>
-          <b-col cols="12">About you:</b-col>
+          <b-col cols="12">Mail</b-col>
           <b-col cols="12">
-            <textarea
+            <input
               class="form-control"
               rows="4"
-              v-model="appUser.id"
+              type="text"
+              v-model="appUser.email"
               @change="updateProfile"
-            ></textarea>
+            />
           </b-col>
         </b-row>
       </div>
     </div>
-
-    <b-modal id="modal-center" centered title="Select your new avatar">
-      <b-row>
-        <b-col
-          v-for="(avatar, index) of avatars"
-          v-bind:key="index"
-          sm="3"
-          class="avatar-menu-inner"
-        >
-          <img @click="selectAvatar(avatar)" :src="/images/ + avatar" />
-        </b-col>
-      </b-row>
-    </b-modal>
   </b-col>
 </template>
 
@@ -66,47 +46,18 @@ import { mapState, mapActions } from "vuex";
 export default {
   name: "UserProfile",
   data: function () {
-    return {
-      avatarMenuVisible: false,
-      avatars: [
-        "avatar1.png",
-        "avatar2.png",
-        "avatar3.png",
-        "avatar4.png",
-        "avatar5.png",
-        "avatar6.png",
-      ],
-    };
+    return {};
   },
   computed: {
     ...mapState({
       appUser: (state) => state.appUser,
     }),
-    avatarPath: function () {
-      return "";
-    },
   },
   methods: {
     //...mapActions(["saveProfile"]),
-    avatarMenu() {
-      var div = document.getElementsByClassName("avatar-menu")[0];
-      if (!this.avatarMenuVisible) {
-        div.style.bottom = "50%";
-        div.style.left = "10%";
-        this.avatarMenuVisible = true;
-      } else {
-        this.avatarMenuVisible = false;
-        div.style.left = "-100%";
-        div.style.bottom = "-100%";
-      }
-    },
     hideProfile() {
       var div = document.getElementsByClassName("side-profile")[0];
       div.style.left = "-100%";
-    },
-    selectAvatar(avatar) {
-      //this.appUser.profile.avatar = avatar;
-      //this.saveProfile();
     },
     updateProfile(event) {
       //this.saveProfile();
@@ -143,5 +94,9 @@ export default {
   border-radius: 50%;
   height: 40px;
   width: 40px;
+}
+
+.compose-sideBar {
+  height: 100%;
 }
 </style>
