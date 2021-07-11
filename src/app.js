328ecd4e667205aa5caf1e7b485122197e6e4c9a
damian <git2507>
2021-07-11 13:22:34 +0200

    Limpieza de codigo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
-//import Bootstrap from 'bootstrap'
 import BootstrapVue from 'bootstrap-vue'
-//import '@babel/polyfill'
-//import 'mutationobserver-shim'
 import './plugins/bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -14,10 +11,6 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-//window.axios = require('axios');
-//window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-//const axios = require('axios');
 
 // My app
 import router from './router/index'
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  // Modifica las rutas usadas por npm build
+  // publicPath: './',
   pages: {
     index: {
       entry: 'src/app.js'
     }
   }
-} 
+}
